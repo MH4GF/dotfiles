98efcc2cb894480d0cccb7800a11dde93a1ae863
MH4GF <rixi436@gmail.com>
2019-09-21 17:31:47 +0900

    add alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,10 @@ alias gh='cd (ghq root)/(ghq list | peco)'
 
 # git
 alias g='git'
+alias push='git push && git delete-merged-branches'
+alias pull='git pull && git delete-merged-branches'
+alias rebase='git fetch && git pull --rebase origin develop'
+
+# linux
+alias ll='ls -la'
 
