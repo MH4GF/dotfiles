6ec85d0319fa322e8d690635edc44fb7babafb3d
MH4GF <rixi436@gmail.com>
2019-09-21 17:36:30 +0900

    add .gitignore_global

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 ln -sf $(pwd)/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.ideavimrc ~/.ideavimrc
+ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
