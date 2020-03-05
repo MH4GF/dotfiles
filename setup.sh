6cc9af3f0f727f63abd2bd0792d868b13b758cd3
MH4GF <rixi436@gmail.com>
2020-03-05 18:13:59 +0900

    add karabiner-elements settings

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 mkdir -p ~/.config/fish
 ln -sf $(pwd)/.config/fish/config.fish ~/.config/fish/config.fish
+
+mkdir -p ~/.config/karabiner/
+ln -sf $(pwd)/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.ideavimrc ~/.ideavimrc
 ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
