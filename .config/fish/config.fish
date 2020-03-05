00f26f42cd739ed04e563230d90a6c9a0d1bacf7
MH4GF <rixi436@gmail.com>
2020-03-05 18:08:11 +0900

    remove alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,6 @@ alias gh='cd (ghq root)/(ghq list | peco)'
 alias g='git'
 alias push='git push && git delete-merged-branches'
 alias pull='git pull && git delete-merged-branches'
-alias rebase='git fetch && git pull --rebase origin develop'
 
 # linux
 alias ll='ls -la'
