714944a1a6a2870d1da88a44f460ab8e5effba56
MH4GF <rixi436@gmail.com>
2021-03-26 14:37:41 +0900

    rm .zshrc

--- .zshrc
+++
@@ -1,1 +0,0 @@
-source ~/.zsh_profile
--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.config/karabiner/
 ln -sf $(dirname ${0})/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 
 ln -sf $(dirname ${0})/.vimrc ~/.vimrc
-ln -sf $(dirname ${0})/.zshrc ~/.zshrc
 ln -sf $(dirname ${0})/.zsh_profile ~/.zsh_profile
 ln -sf $(dirname ${0})/.ideavimrc ~/.ideavimrc
 ln -sf $(dirname ${0})/.gitignore_global ~/.gitignore_global
