7e6d52afc55bed509e8c9add52e13511f0e1c18f
MH4GF <h.miyagi.cnw@gmail.com>
2022-08-14 17:26:11 +0900

    feat: add .ssh/config with 1password
    ref: https://developer.1password.com/docs/ssh/manage-keys/

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ ln -sf $(dirname ${0})/.config/karabiner/karabiner.json ~/.config/karabiner/kara
 ln -sf $(dirname ${0})/.config/starship.toml ~/.config/starship.toml
 ln -sf $(dirname ${0})/.config/gh/config.yml ~/.config/gh/config.yml
 
+mkdir -p ~/.ssh
+ln -sf $(dirname ${0})/.ssh/config ~/.ssh/config
+
 ln -sf $(dirname ${0})/.vimrc ~/.vimrc
 ln -sf $(dirname ${0})/.zshrc ~/.zshrc
 ln -sf $(dirname ${0})/.ideavimrc ~/.ideavimrc
