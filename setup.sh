e4904944efe28dc32604e4e103363251b1b67a37
MH4GF <h.miyagi.cnw@gmail.com>
2023-09-22 14:08:50 +0900

    fix: gitignore_global をデフォルトの .config/git/ignore に変更

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 mkdir -p ~/.config/karabiner/
+mkdir -p ~/.config/git/
+
 ln -sf $(dirname ${0})/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -sf $(dirname ${0})/.config/starship.toml ~/.config/starship.toml
 ln -sf $(dirname ${0})/.config/gh/config.yml ~/.config/gh/config.yml
+ln -sf $(dirname ${0})/.config/git/ignore ~/.config/git/ignore
 
 mkdir -p ~/.ssh
 ln -sf $(dirname ${0})/.ssh/config ~/.ssh/config
@@ -11,6 +14,5 @@ ln -sf $(dirname ${0})/.ssh/config ~/.ssh/config
 ln -sf $(dirname ${0})/.vimrc ~/.vimrc
 ln -sf $(dirname ${0})/.zshrc ~/.zshrc
 ln -sf $(dirname ${0})/.ideavimrc ~/.ideavimrc
-ln -sf $(dirname ${0})/.gitignore_global ~/.gitignore_global
 ln -sf $(dirname ${0})/.gitconfig ~/.gitconfig
 ln -sf $(dirname ${0})/.tool-versions ~/.tool-versions
