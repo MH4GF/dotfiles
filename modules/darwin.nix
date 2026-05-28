a7c1103643a2a0c470eb267b6826b01dd67f0d4e
MH4GF <h.miyagi.cnw@gmail.com>
2026-05-28 13:22:04 +0900

    fix(nix): restore gpg-agent default/max-cache-ttl preserved from brew

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -13,6 +13,8 @@
 
       ".gnupg/gpg-agent.conf".text = ''
         pinentry-program ${pkgs.pinentry_mac}/bin/pinentry-mac
+        default-cache-ttl 600
+        max-cache-ttl 7200
       '';
     };
 
